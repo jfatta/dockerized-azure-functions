b868ae21a974fbafaa678b8ce47d2dda9ab0302f
Jorge L. Fatta <jorge.fatta@southworks.net>
2018-04-25 07:37:46 -0300

    Running locally

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,16 @@
 FROM microsoft/azure-functions-runtime:2.0.0-jessie
 ENV AzureWebJobsScriptRoot=/home/site/wwwroot
 
-# Add files from this repo to the root site folder.
+# install chrome rather than relying on Puppeteer 
+RUN apt-get update && apt-get install -y wget --no-install-recommends \
+    && wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - \
+    && sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' \
+    && apt-get update \
+    && apt-get install -y google-chrome-unstable --no-install-recommends \
+    && rm -rf /var/lib/apt/lists/* \
+    && apt-get purge --auto-remove -y curl \
+    && rm -rf /src/*.deb
+
+ENV PUPPETEER_SKIP_CHROMIUM_DOWNLOAD true
+
 COPY . /home/site/wwwroot 
-
-
--- HttpTriggerJS1/index.js
+++ HttpTriggerJS1/index.js
@@ -1,4 +1,11 @@
-module.exports = function (context, req) {
+const puppeteer = require('puppeteer');
+
+const puppeteerOptions = {
+    executablePath: 'google-chrome-unstable',
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+};
+
+module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     if (req.query.name || (req.body && req.body.name)) {
@@ -6,12 +13,40 @@ module.exports = function (context, req) {
             // status: 200, /* Defaults to 200 */
             body: "Hello " + (req.query.name || req.body.name)
         };
-    }
-    else {
+
+        context.log("launching puppeteer...");
+        const browser = await puppeteer.launch( puppeteerOptions ); // skip options if running outside the docker container
+        const page = await browser.newPage();
+        await page.goto('https://azure.microsoft.com/en-us/', {
+            waitUntil: 'networkidle2'
+        });
+
+        await page.emulateMedia('screen');
+
+        await page.pdf({
+            path: 'result.pdf',
+            format: 'letter',
+            printBackground: true
+        });
+
+        context.log("Closing puppeteer browser...");
+        await browser.close();
+
+        context.res = {
+            status: 200,
+            body: '<root>body</root>',
+            headers: {
+                'Content-Type': 'text/xml'
+            },
+            isRaw: true
+        }
+
+    } else {
         context.res = {
             status: 400,
             body: "Please pass a name on the query string or in the request body"
         };
     }
-    context.done();
+
+    //context.done();
 };
